58044edaf6f3d7b6c00f8505a7c55637d0753e54
mahdi-jafaree <mehdi.jafaree1375@gmail.com>
2022-02-23 20:59:51 +0430

    starting mongodb

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-practice', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
